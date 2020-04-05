fd459915806238ebe0114d2624169d6424982f7f
Nikhil Bagde <nbagde1@binghamton.edu>
2020-04-04 19:57:19 -0500

    Data Structure and Algorithm Revision Chapter 1 Execercise

--- src/Books/DataStructureAndAlgorithms/Ch1/Excercise5.java
+++ src/Books/DataStructureAndAlgorithms/Ch1/Excercise5.java
@@ -11,6 +11,16 @@ public class Excercise5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BigInteger bigInteger = new BigInteger(String.valueOf(scanner.nextInt()));
+        /*
+        Sum of N integer = n*(n+1)/2
+        e.g. 5
+        1 + 2 + 3 + 4 + 5 = 15
+        5*6/2=15
+
+        E.g. 7
+        1+2+3+4+5+6+7= 28
+        7*8/2=28
+         */
         System.out.println(bigInteger.multiply(bigInteger.add(new BigInteger("1"))).divide(new BigInteger("2")));
     }
 }
--- src/Books/DataStructureAndAlgorithms/Ch1/Excercise6.java
+++ src/Books/DataStructureAndAlgorithms/Ch1/Excercise6.java
@@ -1,12 +1,50 @@
 package Books.DataStructureAndAlgorithms.Ch1;
 
+import java.util.Scanner;
+
 /*
 R-1.6 Write a short Java method that takes an integer n and returns the sum of all the
 odd positive integers less than or equal to n.
  */
 public class Excercise6 {
     public static void main(String[] args) {
-        /*2 + 4 + 6 + 8 + 10 = 30
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n == 0) {
+            System.out.println(0);
+        } else if (n == 1) {
+            System.out.println(1);
+        } else {
+            /*
+             if n is even = it will have n/2 odd numbers
+             E.g 8 = 1 3 5 7 = 4 odd numbers.                       8/2=4       total =  4 * 4 = 16
+             E.g 9 = 1 3 5 7 9 = 5 odd numbers =                    9/2=4+1     total = 25 = 5 * 5
+             E.g. 25 = 1 3 5 7 9 11 13 15 17 19 21 23 25 = 13 =     25/2 + 1    total = 169 = 13 * 13
+             */
+
+            if (n % 2 == 0) {
+                System.out.println((n / 2) * (n / 2));
+            } else {
+                System.out.println((n / 2 + 1) * (n / 2 + 1));
+            }
+        }
+
+         /*
+            =1+3+5+7
+            =1+(2+1)+(3+2)+(4+3)          we can write again as..
+            =[1+2+3+4] +[1+2+3]
+            =[Sum of n natural numbers]+[Sum of (n-1) natural numbers]
+            =n(n+1)/2 + (n-1) [(n-1)+1]/2    By solving it,we find
+            =[n^2+n]/2+[n^2-n]/2      again we can simplify as
+            =(2n^2)/2
+            =n^2
+         */
+    }
+
+}
+
+
+/*2 + 4 + 6 + 8 + 10 = 30
         1  =  1
         1 +  2 =  3
         1 + 2 + 3 =  6
@@ -20,6 +58,3 @@ public class Excercise6 {
      6   1 3 5 7 9 11 = 36 = 2^6
      7   1 3 5 7 9 11 13 = 49
         */
-    }
-
-}
